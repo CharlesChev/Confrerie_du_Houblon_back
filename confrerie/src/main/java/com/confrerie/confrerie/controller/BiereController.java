81d1d0fa51cf3a14af30ed54cb63b727ffc56f91
CharlesTravail <charles.chevreuil@gmail.com>
2023-11-19 21:21:42 -0500

    test

--- confrerie/src/main/java/com/confrerie/confrerie/controller/BiereController.java
+++ confrerie/src/main/java/com/confrerie/confrerie/controller/BiereController.java
@@ -1,5 +1,54 @@
 package com.confrerie.confrerie.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.confrerie.confrerie.model.Biere;
+import com.confrerie.confrerie.service.BiereService;
+
+@RestController
 public class BiereController {
+
+    @Autowired
+    private BiereService biereService;
+
+    @GetMapping("/bieresAdoubadidou")
+    public ResponseEntity < List < Biere >> getAllBiere() {
+        return ResponseEntity.ok().body(biereService.getAllBiere());
+    }
+
+    @GetMapping("/bieres/{id}")
+    public ResponseEntity < Biere > getBiereById(@PathVariable long id) {
+        return ResponseEntity.ok().body(biereService.getBiereById(id));
+    }
+
+    @PostMapping("/bieres")
+    public ResponseEntity < Biere > createBiere(@RequestBody Biere biere) {
+        return ResponseEntity.ok().body(this.biereService.createBiere(biere));
+    }
+
+    @PutMapping("/bieres/{id}")
+    public ResponseEntity < Biere > updateBiere(@PathVariable long id, @RequestBody Biere biere) {
+        biere.setId(id);
+        return ResponseEntity.ok().body(this.biereService.updateBiere(biere));
+    }
+
+    @DeleteMapping("/bieres/{id}")
+    public HttpStatus deleteBiere(@PathVariable long id) {
+        this.biereService.deleteBiere(id);
+        return HttpStatus.OK;
+    }
+
+    
     
 }
